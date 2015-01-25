6f483ff1eaab13aee06c74250eb72ea773f13494
wilFin1 <william_fintel@hotmail.com>
2015-01-24 20:28:45 -0500

    Made changes to incorporate the required functionality for the matrix inverse helper functions all contained in the same .R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##	This is the maxtrix structure which will provide the
+##	inverse from the data structure	
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- solve(x)
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+##	This is the maxtrix structure which will generate
+##	an inverse and store both into the data structure
+##	This needs to be adjusted to account for non-invertable
+##	matrices.
+##	Note:	The solve function will default to the inverse
+##	if the second input parameter is missing.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
